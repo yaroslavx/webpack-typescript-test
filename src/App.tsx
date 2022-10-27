fd60e3e3ec7607aeb2aa4f054ac1591adffa3518
Yaroslav Yakupov <yakupov.yaroslav@icloud.com>
2022-10-27 20:11:55 +0300

    prod and dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './styles.css'
 import img from './logo.png'
+import ClickCounter from './ClickCounter'
 
 export const App = () => {
     return <>
-        <h1>Typescript Webpack React</h1>
+        <h1>Typescript Webpack React – {process.env.NODE_ENV} – {process.env.name}</h1>
         <img src={img} alt="logo" width='500' />
+        <ClickCounter />
     </>
 }
