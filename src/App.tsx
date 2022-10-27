3e10e93e6240f71c5e9018e4012312b36bb8c57c
Yaroslav Yakupov <yakupov.yaroslav@icloud.com>
2022-10-27 20:30:30 +0300

    eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import React from 'react'
 import './styles.css'
 import img from './logo.png'
 import ClickCounter from './ClickCounter'
 
 export const App = () => {
+    const name = 'me'
     return <>
         <h1>Typescript Webpack React – {process.env.NODE_ENV} – {process.env.name}</h1>
         <img src={img} alt="logo" width='500' />
