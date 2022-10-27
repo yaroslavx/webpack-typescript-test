f9e2a8aa058cf9d3775abba4c2da0c5c84c98fcf
Yaroslav Yakupov <yakupov.yaroslav@icloud.com>
2022-10-27 16:45:27 +0300

    init

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom/client';
+import { App } from "./App";
+
+const rootElement = document.querySelector('#root')
+
+if (rootElement) {
+    const root = ReactDOM.createRoot(rootElement)
+    root.render(
+        <App />
+    );
+}
