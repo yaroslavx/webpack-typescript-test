7774e6229fd7df025ae4d6462da43d291403df1e
Yaroslav Yakupov <yakupov.yaroslav@icloud.com>
2022-10-27 20:37:18 +0300

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,14 @@ import img from './logo.png'
 import ClickCounter from './ClickCounter'
 
 export const App = () => {
-    const name = 'me'
-    return <>
-        <h1>Typescript Webpack React – {process.env.NODE_ENV} – {process.env.name}</h1>
-        <img src={img} alt="logo" width='500' />
-        <ClickCounter />
+  const name = 'me'
+  return (
+    <>
+      <h1>
+        Typescript Webpack React – {process.env.NODE_ENV} – {process.env.name}
+      </h1>
+      <img src={img} alt="logo" width="500" />
+      <ClickCounter />
     </>
+  )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { App } from './App'
 
 const rootElement = document.querySelector('#root')
 
 if (rootElement) {
-    const root = ReactDOM.createRoot(rootElement)
-    root.render(
-        <App />
-    );
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(<App />)
 }
